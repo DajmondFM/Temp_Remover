254e7f7761c3afff474dd9bcc0d14a3aadf4209b
DajmondFM <franekmalikowski@gmail.com>
2023-08-01 12:25:26 +0200

    add linux

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,23 @@ use std::process::Command;
 extern crate whoami;
 
 fn main() {
-    let name = whoami::username();
-    let path = format!("C:\\Users\\{name}\\AppData\\Local\\Temp");
-    Command::new("cmd")
-        .args(&["/c", "rmdir", "/s", "/q", "{}", &path])
-        .spawn()
-        .expect("Failed to delete C:\\Users\\frane\\AppData\\Local\\Temp");
-    Command::new("cmd")
-        .args(&["/c", "rmdir", "/s", "/q", "C:\\Windows\\Temp"])
-        .spawn()
-        .expect("Failed to delete C:\\Windows\\Temp");
+    if cfg!(target_os = "windows") {
+        let name = whoami::username();
+        let path = format!("C:\\Users\\{name}\\AppData\\Local\\Temp");
+        Command::new("cmd")
+            .args(&["/c", "rmdir", "/s", "/q", &path])
+            .spawn()
+            .expect("Failed to delete C:\\Users\\frane\\AppData\\Local\\Temp");
+        Command::new("cmd")
+            .args(&["/c", "rmdir", "/s", "/q", "C:\\Windows\\Temp"])
+            .spawn()
+            .expect("Failed to delete C:\\Windows\\Temp");
+    } else if cfg!(target_os = "linux") {
+        Command::new("rm")
+            .args(&["-rf", "/tmp/*"])
+            .spawn()
+            .expect("Failed to delete /tmp");
+    }
 }
 
 
